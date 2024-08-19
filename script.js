9fd5f293a4417378075c51503fa24630fa6267e0
Joshmar Morales <privatelampas@gmail.com>
2024-08-18 18:21:58 -0700

    Returned button in visual box to flip description-box and photobox

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 "use strict";
 
-// Updates year in the footer
+// FOOTER FUNCTIONALITY
 const footerDate = document.querySelector(".footer-date");
 const date = new Date();
 footerDate.textContent = `© ${date.getFullYear()}`;
 
 // EXPERIENCE FUNCTIONALITY
 const experiences = document.querySelectorAll(".experience__title");
-
-// const workExperienceBox = document.querySelector(
-//   ".experience__visual-descriptionbox-work"
-// );
-// const technologiesExperience = document.querySelector(
-//   ".experience__visual-descriptionbox-technologies"
-// );
-// const educationExperience = document.querySelector(
-//   ".experience__visual-descriptionbox-education"
-// );
-// const languagesExperience = document.querySelector(
-//   ".experience__visual-descriptionbox-languages"
-// );
 
 const experiencePhotoBox = document.querySelector(
   ".experience__visual-photobox"
@@ -29,45 +16,57 @@ const experienceImage = document.getElementById("experienceImgEl");
 const experienceDescriptionBox = document.querySelector(
   ".experience__visual-descriptionbox"
 );
+const experienceItems = document.querySelectorAll(
+  ".experience__visual-descriptionbox-item"
+);
 const experienceDescription = document.querySelector(
   ".experience__visual-descriptionbox-description"
 );
 
-const expandButton = document.querySelector(".btn");
+const expandButton = document.querySelector(".experience__visual-button");
 const photoBoxExpand = "experience__visual-photobox--expand";
 const descriptionBoxExpand = "experience__visual-descriptionbox--expand";
+let currentExperience = "default";
 
 experiences.forEach(function (experience) {
   experience.addEventListener("mouseover", function (e) {
     e.preventDefault();
-    console.log(experience.textContent);
-
-    experiencePhotoBox.classList.remove(photoBoxExpand);
-    experienceDescriptionBox.classList.remove(descriptionBoxExpand);
+    expandButton.textContent = `Open details`;
+    flipCard(0);
     experienceImage.src = `./assets/svg/${experience.textContent.toLowerCase()}.svg`;
-    const experienceItems = document.querySelectorAll(
-      ".experience__visual-descriptionbox-item"
-    );
+    currentExperience = experience.textContent.toLowerCase();
+    // Hide all experiences again
     experienceItems.forEach((item) => item.classList.add("u-hidden"));
-  });
-
-  experience.addEventListener("click", function (e) {
-    e.preventDefault();
-    experiencePhotoBox.classList.add(photoBoxExpand);
-    experienceDescriptionBox.classList.add(descriptionBoxExpand);
-    const selected = experience.textContent.toLowerCase();
-    console.log(selected);
-
-    document
-      .querySelector(`.experience__visual-descriptionbox-${selected}`)
-      .classList.remove("u-hidden");
   });
 });
 
-// expandButton.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log(`Expand button clicked`);
-//   experiencePhotoBox.classList.add(photoBoxExpand);
-//   experienceDescriptionBox.classList.add(descriptionBoxExpand);
-//   experienceDescription.textContent = selectedDescription;
-// });
+function flipCard(page) {
+  if (page === 0) {
+    experiencePhotoBox.classList.remove(photoBoxExpand);
+    experienceDescriptionBox.classList.remove(descriptionBoxExpand);
+  } else if (page === 1) {
+    // Only show text for recently hovered experience
+    document
+      .querySelector(`.experience__visual-descriptionbox-${currentExperience}`)
+      .classList.remove("u-hidden");
+    experiencePhotoBox.classList.add(photoBoxExpand);
+    experienceDescriptionBox.classList.add(descriptionBoxExpand);
+  }
+}
+
+expandButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const state = expandButton.textContent.split(" ")[0].toLowerCase();
+  if (state === "close") {
+    flipCard(0);
+    expandButton.textContent = `Open details`;
+    setTimeout(function () {
+      // Hide all experiences again
+      experienceItems.forEach((item) => item.classList.add("u-hidden"));
+    }, 300);
+    return;
+  }
+
+  flipCard(1);
+  expandButton.textContent = `Close details`;
+});
