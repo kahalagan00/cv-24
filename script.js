a9c62215911970441384d3f4110ff473e4f4b931
Joshmar Morales <privatelampas@gmail.com>
2024-08-21 16:49:34 -0700

    Improved experience list selection interactivity and responsiveness. Improved card flip responsiveness.

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const expDescriptionBox = document.querySelector(
 const expDescriptions = document.querySelectorAll(".experience__description");
 const expandButton = document.querySelector(".experience__button");
 
+const expLotties = document.querySelectorAll(".experience__description-lottie");
+
 let specific_exp = `default`;
 
 expandButton.addEventListener("click", function (e) {
@@ -23,37 +25,61 @@ expandButton.addEventListener("click", function (e) {
 
   if (expandButton.textContent[0] === "O") {
     expandButton.textContent = "Close details";
-  } else expandButton.textContent = "Open details";
+
+    setTimeout(() => {
+      document
+        .querySelector(`.experience__description-${specific_exp}`)
+        .classList.add("experience__description--active");
+      expLotties.forEach((lottie) =>
+        lottie.classList.remove("experience__description-lottie-hidden")
+      );
+    }, 200);
+  } else {
+    setTimeout(() => {
+      expLotties.forEach((lottie) =>
+        lottie.classList.add("experience__description-lottie-hidden")
+      );
+      expDescriptions.forEach((description) =>
+        description.classList.remove("experience__description--active")
+      );
+    }, 200);
+
+    expandButton.textContent = "Open details";
+  }
 
   expPhotoBox.classList.toggle("experience__visual-photobox--expand");
   expDescriptionBox.classList.toggle(
     "experience__visual-descriptionbox--expand"
   );
-
-  expDescriptions.forEach((description) =>
-    description.classList.remove("experience__description--active")
-  );
-
-  document
-    .querySelector(`.experience__description-${specific_exp}`)
-    .classList.add("experience__description--active");
 });
 
 experiences.forEach(function (exp) {
-  exp.addEventListener("mouseover", function (e) {
+  exp.addEventListener("click", function (e) {
     e.preventDefault();
+    experiences.forEach(function (exp2) {
+      if (exp2 !== exp) {
+        exp2.classList.remove("experience__list-item--active");
+      }
+    });
+    exp.classList.add("experience__list-item--active");
+
+    expandButton.textContent = "Open details";
     expPhotoBox.classList.remove("experience__visual-photobox--expand");
     expDescriptionBox.classList.remove(
       "experience__visual-descriptionbox--expand"
     );
+
     specific_exp = exp.firstElementChild.textContent
       .toLowerCase()
       .split(" ")
       .join("")
       .split("\n")
       .join("");
+
     if (specific_exp === "experience") specific_exp = "default";
+
     console.log(`<--${specific_exp}-->`);
+
     expImage.src = `/assets/svg/${specific_exp}.svg`;
   });
 });
